126d5eabfee62ef5aacd94c885f4e0eb58fe6150
Maloochow <luyao.mly@gmail.com>
2020-06-10 19:44:00 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,27 @@
+class Doctor
+
+attr_reader :name
+@@all = []
+
+def self.all
+    @@all
+end
+
+def initialize(name)
+    @name = name
+    @@all << self
+end
+
+def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+end
+
+def appointments
+    Appointment.all.select{|a| a.doctor == self}
+end
+
+def patients
+    appointments.map {|a| a.patient}
+end
+
+end
