663fc3f0c229a5b48d0fce5ef281e3ed11110ec0
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-06-10 18:08:36 -0600

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,34 @@
-class Artist
+class Artist 
+    attr_accessor :name, :genre, :song
+    
     @@all = []
-
-    attr_accessor :all
-    def initialize()
-        @@all << self
+    
+    def initialize(name)
+      @name = name
+      @@all << self
+      
+    end 
+    
+    def self.all
+      @@all
+    end
+    
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+      
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+        song.artist = self 
+        song 
+    
+    end
+    def genres
+        songs.map do |song|
+            song.genre
+        end
+  
     end
 
-
-end
+  end
+  
